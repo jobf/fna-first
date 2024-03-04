d54881516edf2c1224085cc88344a71edb473a92
jf <22012032+jobf@users.noreply.github.com>
2024-03-04 13:31:57 +0000

    simplest test

--- FnaFirst.Console/Program.cs
+++ FnaFirst.Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Xna.Framework;
+
+static class Program
+{
+	[STAThread]
+	static void Main(string[] args)
+	{
+		using (Game g = new Game())
+		{
+			new GraphicsDeviceManager(g);
+			g.Run();
+		}
+	}
+}
